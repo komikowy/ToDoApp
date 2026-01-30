63abdfa620933513707d6a3b24086f74783716bf
komikowy <komikowy@gmail.com>
2026-01-30 02:06:03 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-import { TodoStore } from '../js/store/uiStore.js';
-import { UIStore } from '../js/store/uiStore.js';
-import { NotificationService } from '../js/services/notificationService.js';
-import { TodoView } from '../js/view/todoView.js';
-import { TodoController } from '../js/controllers/todoController.js';
+import { TodoStore } from '../store/uiStore.js';
+import { UIStore } from '../store/uiStore.js';
+import { NotificationService } from '../services/notificationService.js';
+import { TodoView } from '../view/todoView.js';
+import { TodoController } from '../controllers/todoController.js';
 
 export function initApp() {
     console.log("Inicjalizacja Systemu...");
